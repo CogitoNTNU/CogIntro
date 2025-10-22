947ee8940aeb1e3f69eacb770a0c1f40ce2d16d1
svemyh <s-myhre@outlook.com>
2025-10-22 22:51:26 +0200

    feat: add autopet NAIC dataset example

---
+++ src/tumor-segmentation/data/README-autopet.md
@@ -0,0 +1,10 @@
+# Documentation: autopet
+
+"Whole-body MIP-PET" images of patients with cancer.
+
+## Origin
+
+`autopet` contains the training data from the Norwegian AI Championship 2025.
+
+Further information provided at:
+https://github.com/amboltio/DM-i-AI-2025/tree/main/tumor-segmentation
--- src/tumor-segmentation/main.py
+++ src/tumor-segmentation/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import json
 import os
 from pathlib import Path
 import wandb
-import nibabel as nib
 
 # Loads the variables from the .env file into the runtime environment
 from dotenv import load_dotenv
@@ -15,46 +13,24 @@ load_dotenv()
 def main():
     print("Script started")
 
-    dataset_path = Path("data/psma-fdg-pet-ct-lesions/dataset.json")
-    with open(dataset_path, "r") as f:
-        dataset_info = json.load(f)
-
-    print(f"Loaded dataset: {dataset_info['name']}")
-
     # Load environment variables (from the .env file)
     wandb_api_key = os.getenv("WANDB_API_KEY")
     wandb_username = os.getenv("WANDB_ENTITY")
 
     wandb.login(key=wandb_api_key)
 
-    wandb.init(project="tumor-segmentation", entity=wandb_username, config=dataset_info)
+    wandb.init(project="tumor-segmentation", entity=wandb_username)
+
+    # Log patient image and segmentation
+    patient_image_path = Path("data/autopet/patients/imgs/patient_000.png")
+    segmentation_path = Path("data/autopet/patients/labels/segmentation_000.png")
 
     wandb.log(
         {
-            "dataset_name": dataset_info["name"],
-            "num_training_samples": dataset_info["numTraining"],
-            "num_labels": len(dataset_info["labels"]),
+            "patient_image": wandb.Image(str(patient_image_path)),
+            "segmentation": wandb.Image(str(segmentation_path)),
         }
     )
-
-    # Load and display one of the images in the W&B dashboard
-    image_path = Path(
-        "data/psma-fdg-pet-ct-lesions/imagesTr/psma_ffcaa75377465b37_2018-03-04_0001.nii.gz"
-    )
-    nifti_image = nib.load(image_path)
-    image_data = nifti_image.get_fdata()
-
-    print(f"Loaded image: {image_path.name}, shape: {image_data.shape}")
-
-    # Log a few slices
-    middle_slice = image_data[:, :, image_data.shape[2] // 2 + 40]
-    wandb.log({"sample_image1": wandb.Image(middle_slice, caption=image_path.name)})
-
-    middle_slice = image_data[:, :, image_data.shape[2] // 2 + 50]
-    wandb.log({"sample_image2": wandb.Image(middle_slice, caption=image_path.name)})
-
-    middle_slice = image_data[:, :, image_data.shape[2] // 2 + 60]
-    wandb.log({"sample_image3": wandb.Image(middle_slice, caption=image_path.name)})
 
     wandb.finish()
 
