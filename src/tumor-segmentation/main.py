5e682ecc812a6be5b164289ecd865ea81c4648d8
svemyh <s-myhre@outlook.com>
2025-10-28 19:09:53 +0100

    wip

--- src/tumor-segmentation/main.py
+++ src/tumor-segmentation/main.py
@@ -563,6 +563,64 @@ def get_dice(preds, masks, threshold=0.5, epsilon=1e-6):
     return dice_scores.mean()
 
 
+class BuildDataset(torch.utils.data.Dataset):
+    def __init__(self,
+                df,
+                transforms=None):
+
+        self.df           = df.reset_index(drop=True)
+        self.transforms   = transforms
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        img_path = self.df.image_path[index]
+        label_path = self.df.label_path[index]
+
+        ## Load the image (RGB)
+        img, mask = load_img(img_path, label_path, True)
+        ## Apply Augmentations:
+        if self.transforms:
+            data = self.transforms(image=img, mask=mask)
+            img  = data['image']
+            mask = data['mask']
+            img = np.transpose(img, (2, 0, 1))
+        else:
+            img = np.transpose(img, (2, 0, 1))
+
+        mask = np.transpose(mask, (2, 0, 1))
+
+        # if CFG.aux_head and self.label:
+        #     labels = np.where(mask.sum((1, 2)) > 0, 1, 0)
+        # else:
+        #     labels = mask
+        img = torch.tensor(img)
+        mask = torch.tensor(mask)
+        return torch.tensor(img), torch.tensor(mask), img_path
+
+
+class TTADataset(torch.utils.data.Dataset):
+    def __init__(self, base_dataset, tta_transforms):
+        self.base_dataset = base_dataset
+        self.tta_transforms = tta_transforms
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def __getitem__(self, idx):
+        image, mask, img_path = self.base_dataset[idx]
+
+        image = image.permute(1, 2, 0).numpy()
+
+        all_aug_images = []
+        for t in self.tta_transforms:
+            aug = t(image=image)['image']
+            aug = torch.from_numpy(aug).permute(2, 0, 1).float()  # back to CHW
+            all_aug_images.append(aug)
+
+        return torch.stack(all_aug_images), mask, img_path
+
+
 def prepare_loaders(df, data_transforms, fold, non_empty=False):
     train_df = df[df.fold != fold].reset_index(drop=True)
     valid_df = df[df.fold == fold].reset_index(drop=True)
@@ -728,62 +786,6 @@ def main():
 
 
 
-    class BuildDataset(torch.utils.data.Dataset):
-        def __init__(self,
-                    df,
-                    transforms=None):
-
-            self.df           = df.reset_index(drop=True)
-            self.transforms   = transforms
-        def __len__(self):
-            return len(self.df)
-
-        def __getitem__(self, index):
-            img_path = self.df.image_path[index]
-            label_path = self.df.label_path[index]
-
-            ## Load the image (RGB)
-            img, mask = load_img(img_path, label_path, True)
-            ## Apply Augmentations:
-            if self.transforms:
-                data = self.transforms(image=img, mask=mask)
-                img  = data['image']
-                mask = data['mask']
-                img = np.transpose(img, (2, 0, 1))
-            else:
-                img = np.transpose(img, (2, 0, 1))
-
-            mask = np.transpose(mask, (2, 0, 1))
-
-            # if CFG.aux_head and self.label:
-            #     labels = np.where(mask.sum((1, 2)) > 0, 1, 0)
-            # else:
-            #     labels = mask
-            img = torch.tensor(img)
-            mask = torch.tensor(mask)
-            return torch.tensor(img), torch.tensor(mask), img_path
-
-
-    class TTADataset(torch.utils.data.Dataset):
-        def __init__(self, base_dataset, tta_transforms):
-            self.base_dataset = base_dataset
-            self.tta_transforms = tta_transforms
-
-        def __len__(self):
-            return len(self.base_dataset)
-
-        def __getitem__(self, idx):
-            image, mask, img_path = self.base_dataset[idx]
-
-            image = image.permute(1, 2, 0).numpy()
-
-            all_aug_images = []
-            for t in self.tta_transforms:
-                aug = t(image=image)['image']
-                aug = torch.from_numpy(aug).permute(2, 0, 1).float()  # back to CHW
-                all_aug_images.append(aug)
-
-            return torch.stack(all_aug_images), mask, img_path
 
 
 
